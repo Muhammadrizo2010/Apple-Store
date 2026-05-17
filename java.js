9e3c91ebddaca8335db24b57b0af937dd0a69a1a
MuhammadrizoShuhratov <shuhratovmuhammadrizo24@gmail.com>
2026-05-17 12:00:32 +0300

    over

--- java.js
+++ java.js
@@ -21,3 +21,95 @@ function switchTab(type) {
             tabs[0].classList.remove('active');
         }
     }
+
+    async function getStudents() {
+    const response = await fetch('http://localhost:8080/api/students');
+    const data = await response.json(); // Kelgan JSON ma'lumotni o'qiymiz
+    console.log(data);
+    }
+
+
+
+    async function yuklashMahsulotlar() {
+    try {
+        // Backend portingiz 8080 bo'lsa:
+        const response = await fetch('http://localhost:8080/api/products');
+        
+        if (!response.ok) {
+            throw new Error("Ma'lumotni olib bo'lmadi");
+        }
+
+        const mahsulotlar = await response.json();
+        console.log(mahsulotlar); // Konsolda tekshirish
+
+        // Ekranga chiqarish (HTML-da 'products-list' id-li element bo'lishi kerak)
+        const list = document.getElementById('products-list');
+        list.innerHTML = mahsulotlar.map(p => `
+            <div class="card">
+                <h3>${p.name}</h3>
+                <p>Narxi: ${p.price} $</p>
+            </div>
+        `).join('');
+
+    } catch (error) {
+        console.error("Xatolik:", error);
+    }
+}
+
+yuklashMahsulotlar();
+
+
+
+
+
+
+
+
+
+
+
+
+const openBtn = document.getElementById('openMenu');
+const closeBtn = document.getElementById('closeMenu');
+const overlay = document.getElementById('menuOverlay');
+
+// Menyuni ochish
+openBtn.addEventListener('click', () => {
+    overlay.classList.add('active');
+});
+
+// Menyuni yopish
+closeBtn.addEventListener('click', () => {
+    overlay.classList.remove('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  let isOpen = false;
+
+  function toggleMenu() {
+    isOpen = !isOpen;
+    document.getElementById('burgerBtn').classList.toggle('open', isOpen);
+    document.getElementById('mobileMenu').classList.toggle('open', isOpen);
+  }
+
+
+
+
+
+
+
+
+
+
